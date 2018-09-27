d15e55a9b4a45775a62cf59aa5477721ee88b6bd
nguyen minh hieu <minhhieu14351997@gmail.com>
2018-09-27 23:31:06 +0700

    make it better

--- pythonweb/wsgi.py
+++ pythonweb/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythonweb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythonweb.settings")
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 #from dj_static import Cling
