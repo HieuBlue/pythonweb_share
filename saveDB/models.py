e09964baf1689ba046edef04664a32cb59150940
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-02-15 13:32:30 +0700

    make it better

--- saveDB/models.py
+++ saveDB/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Time_Hum_Tem(models.Model):
 	title = models.CharField(max_length=100)
-	body = models.TextField()
+	body = models.TextField(null=True)
 	date = models.DateTimeField(auto_now_add=True)
 def __str__(self):
 	return self.title		
