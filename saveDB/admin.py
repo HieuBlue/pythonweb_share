9ba957bf3b30f6224e0809d8f4809be57be01ff2
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-02-15 13:55:01 +0700

    make it better

--- saveDB/admin.py
+++ saveDB/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Time_Hum_Tem
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-	list_display = ['title', 'body', 'date']
+	list_display = ['topic', 'message', 'date']
 	list_filter = ['date']
-	search_fields = ['title']
+	search_fields = ['topic']
 admin.site.register((Time_Hum_Tem), PostAdmin)
--- saveDB/models.py
+++ saveDB/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Time_Hum_Tem(models.Model):
-	title = models.CharField(max_length=100)
-	body = models.TextField(null=True)
+	topic = models.CharField(max_length=100)
+	message = models.CharField(max_length=500)
 	date = models.DateTimeField(auto_now_add=True)
 def __str__(self):
-	return self.title		
+	return self.topic		
