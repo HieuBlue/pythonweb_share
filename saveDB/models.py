db2d4fe8bb585068b46d1ca8aa8978bd35363ccc
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-02-15 17:07:39 +0700

    make it better

--- saveDB/models.py
+++ saveDB/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-
 # Create your models here.
 class Time_Hum_Tem(models.Model):
 	topic = models.CharField(max_length=100)
 	message = models.CharField(max_length=500)
 	date = models.DateTimeField(auto_now_add=True)
+class store_data():
+	store = Time_Hum_Tem()
+	store.topic = 'Thời Gian-Độ Ẩm-Nhiệt Độ'
+	store.message = '16:37/15/02/19-50-27'
+	store.save()
+
 def __str__(self):
 	return self.topic		
+	store_data()
