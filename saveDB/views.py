d64d71869dec021f65099fced2bb2d2380d1082d
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-02-15 15:41:56 +0700

    make it better

--- saveDB/views.py
+++ saveDB/views.py
@@ -3,4 +3,6 @@ from .models import Time_Hum_Tem# Create your views here.
 def list(request):
 	Data  = {'TimeHumTem': Time_Hum_Tem.objects.all().order_by("-date")}	#neu xep theo gia tri thi :.count()
 	return render(request, 'saveDB/saveDB.html', Data)
-	
+def post(request, id):
+	post = Time_Hum_Tem.objects.get(id=id)
+	return render(request, 'saveDB/saveDB.html', {'post': post})
