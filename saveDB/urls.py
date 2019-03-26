850f1250087bcbdc88af30fe9c24fd5f45b0be13
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-03-26 13:50:40 +0700

    make it better

--- saveDB/urls.py
+++ saveDB/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, re_path
 from . import views
 from django.conf.urls import url
-from saveDB.views import requestAjax
 urlpatterns = [
 	path('', views.list),
 	path('<int:id>/', views.post),
-	path('my_ajax_request', requestAjax, name='my_ajax_request'),
 ]
