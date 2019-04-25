2cbc39d1b025afb269e471eb31c874f5e3ca0fb8
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-04-25 23:00:45 +0700

    make it better

--- saveDB/models.py
+++ saveDB/models.py
@@ -1,27 +1,13 @@
 from django.db import models
 import paho.mqtt.client as mqtt
-# Create your models here.
 class Time_Hum_Tem(models.Model):
-	DATA_MQTT = models.CharField(max_length=200)
+	DATA_MQTT = models.CharField(max_length=500)
 	date = models.DateTimeField(auto_now_add=True)
 	def __str__(self):
-		return self.DATA_MQTT
-
-MQTT_SERVER = "192.168.1.197" #IP broker 
-MQTT_PATH = "Temp-Hum-Time"
-def on_connect1(client, userdata, flags, rc):
-	print("connected with code "+str(rc))
-	client.subscribe(MQTT_PATH)
-	
-def on_message1(client, userdata, msg):
-    print(MQTT_PATH+" "+str(msg.payload))       
+		return self.DATA_MQTT     
 def store_data():
 	store = Time_Hum_Tem()
-	store.DATA_MQTT = "ksdjhfkj"
+	store.DATA_MQTT = 'ksdjhfkj'
 	store.save()
-store_data()
-client = mqtt.Client() 
-client.on_connect = on_connect1
-client.on_message = on_message1
-client.connect(MQTT_SERVER,1883,60)	
-#client.loop_forever()	
+#store_data()
+
--- saveDB/views.py
+++ saveDB/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from .models import Time_Hum_Tem# Create your views here.
+from .models import store_data
+import paho.mqtt.client as mqtt
 def list(request):
 	Data  = {'TimeHumTem': Time_Hum_Tem.objects.all().order_by("-date")}	#neu xep theo gia tri thi :.count()
 	return render(request, 'saveDB/saveDB.html', Data)
 def post(request, id):
 	post = Time_Hum_Tem.objects.get(id=id)
+
+MQTT_SERVER = "192.168.1.197" #IP broker 
+MQTT_PATH = "Temp-Hum-Time"
+def on_connect1(client, userdata, flags, rc):
+	print("connected with code "+str(rc))
+	client.subscribe(MQTT_PATH)
+	
+def on_message1(client, userdata, msg):
+    print(MQTT_PATH+" "+str(msg.payload)) 
+client = mqtt.Client() 
+client.on_connect = on_connect1
+client.on_message = on_message1
+client.connect(MQTT_SERVER,1883,60)	
+#client.loop_forever()		
