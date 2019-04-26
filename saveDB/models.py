6837a1505554a1ba39fb4b74f3c7281b8ce07d69
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-04-26 23:42:50 +0700

    make it better

--- saveDB/models.py
+++ saveDB/models.py
@@ -1,14 +1,10 @@
 from django.db import models
-# Create your models here.
 class Time_Hum_Tem(models.Model):
-	topic = models.CharField(max_length=100)
-	message = models.CharField(max_length=500)
+	DATA_MQTT = models.CharField(max_length=500)
 	date = models.DateTimeField(auto_now_add=True)
 	def __str__(self):
-		return self.topic		
-def store_data():
+		return self.DATA_MQTT     
+def store_data(msg_payload):
 	store = Time_Hum_Tem()
-	store.topic = 'Thời Gian-Độ Ẩm-Nhiệt Độ'
-	store.message = '16:37/15/02/19-50-27'
+	store.DATA_MQTT = msg_payload
 	store.save()
-store_data()
--- saveDB/views.py
+++ saveDB/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Time_Hum_Tem# Create your views here.
+from .models import Time_Hum_Tem
+from .sub_temp_hum import *
 def list(request):
-	Data  = {'TimeHumTem': Time_Hum_Tem.objects.all().order_by("-date")}	#neu xep theo gia tri thi :.count()
+	Data  = {'TimeHumTem': Time_Hum_Tem.objects.all().order_by("-date")}
 	return render(request, 'saveDB/saveDB.html', Data)
 def post(request, id):
 	post = Time_Hum_Tem.objects.get(id=id)
