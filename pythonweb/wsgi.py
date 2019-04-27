568262a41bea17a7fcc82a78d89cda06ec8365d7
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-04-27 12:27:38 +0700

    make it better

--- pythonweb/wsgi.py
+++ pythonweb/wsgi.py
@@ -6,4 +6,4 @@ from saveDB.sub_temp_hum import *
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythonweb.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-client.loop_start()
+#client.loop_start()
