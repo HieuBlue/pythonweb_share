41a223b79c035c8da6408bd27cc6d0a6522eafbf
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-04-29 17:44:13 +0700

    make it better

--- pythonweb/wsgi.py
+++ pythonweb/wsgi.py
@@ -1,9 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-#import paho.mqtt.client as mqtt
-#from saveDB.sub_temp_hum import *
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythonweb.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-#client.loop_start()
