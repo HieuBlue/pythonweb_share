39b7c827d1d178f291641feacd7a79e1023fd265
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-05-23 00:23:45 +0700

    realtime local but not working heroku

--- saveDB/__init__.py
+++ saveDB/__init__.py
@@ -1,4 +1,4 @@
-#import paho.mqtt.client as mqtt
-#from saveDB.sub_temp_hum import *
+import paho.mqtt.client as mqtt
+from saveDB.sub_temp_hum import *
 
-#client.l#oop_start#()
+client.loop_start()
