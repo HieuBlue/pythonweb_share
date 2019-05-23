3a9b1f78ad68bec5fe47efd9bb8caee7872bbe07
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-05-23 11:13:50 +0700

    save() 1 time set loop at views

--- saveDB/views.py
+++ saveDB/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Time_Hum_Tem
-from .sub_temp_hum import *
-import paho.mqtt.client as mqtt
+#from .sub_temp_hum import *
+#import paho.mqtt.client as mqtt
 #from saveDB.sub_temp_hum import *
 
 
