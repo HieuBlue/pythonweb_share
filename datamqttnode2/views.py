068a228c73578a89147d85a31e8ff9a531b96414
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-06-07 02:02:20 +0700

    upgrade frontend

--- datamqttnode2/views.py
+++ datamqttnode2/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Node2
-#from .sub_temp_hum_node2 import *
-#import paho.mqtt.client as mqtt
+from .sub_temp_hum_node2 import *
+import paho.mqtt.client as mqtt
 
 def list(request):
 	Data  = {'TimeHumTem': Node2.objects.all().order_by("-date")[0:1]}
@@ -9,4 +9,4 @@ def list(request):
 def post(request, id):
 	post = Node2.objects.get(id=id)
 
-#client.loop_start()#set at views only save() 1 time
+client.loop_start()#set at views only save() 1 time
