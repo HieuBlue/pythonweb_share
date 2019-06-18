fe3b0ca6058c72183c9d18353031fcee93b87211
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-06-18 10:57:34 +0700

    create 4 button control

--- turnondevicenode2/views.py
+++ turnondevicenode2/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .MQTT_control import *
+#from .MQTT_control import *
 # Create your views here.
 def controldevice(request):
-	publish.single(MQTT_TOPIC,"Node2-ON",hostname=MQTT_SERVER)
+	#publish.single(MQTT_TOPIC,"Node2-ON",hostname=MQTT_SERVER)
 	return render(request,'Turned_on.html')
