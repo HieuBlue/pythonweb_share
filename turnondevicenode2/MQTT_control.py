4eeb43f88fed6a438bf1756d8fe58e04e6925383
nguyen minh hieu <minhhieu14351997@gmail.com>
2019-07-01 10:21:39 +0700

    broker-public

--- turnondevicenode2/MQTT_control.py
+++ turnondevicenode2/MQTT_control.py
@@ -4,7 +4,7 @@ import paho.mqtt.client as mqtt , os
 from time import sleep
 
 client = mqtt.Client()
-MQTT_SERVER = "192.168.1.197" 
+MQTT_SERVER = "broker.hivemq.com" 
 MQTT_TOPIC = "clientSendControl"
 
 
